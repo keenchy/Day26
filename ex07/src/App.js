c217451b632bd9c0c6172fd5f5f61aed2db00e95
keenchy <h.kenan81@hotmail.com>
2021-04-02 23:45:08 +0200

    Axios Post Error Handling

---
+++ ex07/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import PostRequestErrorHandling from "./PostRequestErrorHandling";
+
+function App() {
+    return ( <
+        div className = 'App' >
+        <
+        PostRequestErrorHandling / >
+        <
+        /div>
+    );
+}
+
+export default App;
