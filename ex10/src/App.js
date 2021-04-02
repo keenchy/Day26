5db6a64a327874ef98245bb21107721b93a88ac7
keenchy <h.kenan81@hotmail.com>
2021-04-03 00:54:58 +0200

    Axios Delete Request

---
+++ ex10/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import DeleteRequest from "./DeleteRequest";
+
+function App() {
+    return ( <
+        div className = 'App' >
+        <
+        DeleteRequest / >
+        <
+        /div>
+    );
+}
+
+export default App;
