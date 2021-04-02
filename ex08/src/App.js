fec9971561af68207d6f682b2193867d0bc855bb
keenchy <h.kenan81@hotmail.com>
2021-04-03 00:09:42 +0200

    Axios Post with Header

---
+++ ex08/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import PostRequestSetHeaders from "./PostRequestSetHeaders";
+
+function App() {
+    return ( <
+        div className = 'App' >
+        <
+        PostRequestSetHeaders / >
+        <
+        /div>
+    );
+}
+
+export default App;
