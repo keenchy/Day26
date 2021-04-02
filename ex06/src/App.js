c2ac8944c454db8b874c459a4ca8b1efcaa97e10
keenchy <h.kenan81@hotmail.com>
2021-04-02 23:21:26 +0200

    Post Request Async/Await

---
+++ ex06/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import PostRequestAsyncAwait from "./PostRequestAsyncAwait";
+
+function App() {
+    return ( <
+        div className = 'App' >
+        <
+        PostRequestAsyncAwait / >
+        <
+        /div>
+    );
+}
+
+export default App;
